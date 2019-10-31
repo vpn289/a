7d037cc1223d3f78c000c577d28ebfc19cbfda4b
vpn289 <55871276+vpn289@users.noreply.github.com>
2019-10-31 16:48:45 +0300

    Update pigraphdif.asm

--- pigif/pigraphdif.asm
+++ pigif/pigraphdif.asm
@@ -25,13 +25,112 @@ entry start
 ;convert bitmap to gif blocks
 ;8-bit pixsels to 9-bit gif chanks
 
+            call     enumfile
+            call     draw_graph
+           ; call     eighttonine
+            call       lzwp
+
+;----------------------------
+;here save the  gif
+
+         mov     rcx,[handle_bmp]
+       sub     rsp,40h
+         mov     rdx,gif_file_header
+         mov     r8,54+768   +hdeight*row+4
+         mov     r9,byteswritten
+         mov     qword [rsp+20h],0
+         call    [WriteFile]
+       add     rsp,40h
 
           xor     rcx,rcx
           call    [ExitProcess]
 
           include       'opnepihex.inc'
           include       'enumfilenm.inc'
-          include       '9-8pack.inc'
+;          include       '9-8pack.inc'
+          include       'drawgraph.inc'
+
+;---------------
+;LZW pack
+;óíèâåðñàëüíûé çàïîëíèòåëü
+; ñòðîèì òàáëèöó ññûëîê åíà ïåðâûå 256 êîäîâ
+
+lzwp:
+
+        xor      rcx,rcx
+        mov     rax,lzw_vocab
+
+filler:
+        mov     [rcx*8+lzw_table],rax
+        inc     rax
+        inc     rcx
+        cmp     rcx,255
+        jne     filler
+
+
+lzwpack:
+      ;  int3
+        nop
+        nop
+        nop
+        mov     rsi,pixels
+        mov     r8,pixels ;end of source string
+        mov     rbx,lzw_vocab
+        mov     rdi,compressed_picture
+        mov     rdx,lzw_table
+        mov     r11,lzw_code
+        xor     r10,r10
+        xor     rax,rax
+        mov     r12,1 ; äëèíà èñêîìîé ñòðîêè-1 (òîåñòü 2)
+
+        mov     r13,r12
+
+        mov     r14,lzw_table+256*8+2*8 ;
+        ;ïåðåíîñèì ïåðâûé ñèìâîë íà âûõîä
+        movzx       ax,byte [rsi]
+        mov        [r11],ax
+
+        ;ñðàâíåíèå ñòðîê äåëàåì ÷åðåç cmpsb
+        ;rsi ñòðîêà èñòî÷íèê
+        ;rdi ñòðîêà â ñëîâàðå
+        ;rcx äëèíà ñòðîêè
+        mov  rsi,pixels
+        ;rdi íàäî áðàòü èç lzw_table
+        ;ïî õîðîøåìó ïðîáåãàòü ñëîâàðü íàäî îò êîíöà ê íà÷àëó, òîãäà ïðîâåðêà íà äèíàìè÷åñêèé êîíåö ñòàíîâèòñÿ ïðîâåðêîé íà 0.
+        ;ïîèñê äâóõ è áîëåå çíà÷íûõ ñòðîê çàêàí÷èâàåòñÿ íà êîäå 101
+        rep  cmpsb
+
+
+lzwp3:
+        mov     al,[rsi+r13]      ;÷èòàåì ñèâìîë, åñëè ýòî êîíåö - ïëþåì, èà÷å èùåì ñòðîêó èç ïðåäûóùåãî è òåêóùåãî ñèìâîëà â ñëîâàðå.
+        ;còðîêà ïðîñòèðàåòñÿ îò rsi äî pixels ìàêñèìóì, ... íåíå. ìàêñèìàëüíàÿ äëèíà èñêîìîé ñòðîêè ôèêñèðóåòñÿ ñ÷åò÷èêîì. ñíà÷àëà îíà 2
+
+lzwp2:
+        mov     r9,[lzw_table+r10*8] ; get start of 1-st string
+        cmp     al,[r9+r13]
+        je      lzwp1
+        ;get next entry in lzw table
+        inc     r10
+        jmp     lzwp2
+
+lzwp1:
+        ;íàøëè ñèìâîë ïîèùåì ñëåäóþùèé
+        dec    r13
+        mov    al,[rsi+r13]
+        cmp    al,[r9+r13]
+        jne    lzwp3 ;íå ñîâïàë ñèìâîë, íàäî èñêàòü äàëüøå
+        test   r13,r13 ;à òóò ïðîøëè âñþ ñòðîêó, çíà÷èò íàøëè åå
+        jne    lzwp1  ;íå äî êîíöà ïðîøëè ñòðîêó
+
+       ;òóòî÷êè íàøëè ñòðîêó, âûïëþíåì êîä
+       ;save to lzw_code
+       mov   rax,r9
+       sub   rax,lzw_vocab
+       mov   [r11],ax
+       inc   r11
+       inc   r11
+       ret
+
 
 
 ;-----------------
@@ -54,6 +153,7 @@ bytesreaded     dq      0
 
 
 
+
 heighst:
         dq      0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0
 accumul:
@@ -62,23 +162,57 @@ accumul:
 
 
 pixels:
+        db 7,7,7,7,7,7,7,7,7,7
+        db 7,7,7,7,7,7,7,7,7,7
+        db 7,7,7,7,7,7,7,7,7,7
+        db 7,7,7,7,7,7,7,7,7,7
+        db 7,7,7,7,7,7,7,7,7,7
+        db 7,7,7,7,7,7,7,7,7,7
+        db 7,7,7,7,7,7,7,7,7,7
+        db 7,7,7,7,7,7,7,7,7,7
+        db 7,7,7,7,7,7,7,7,7,7
+        db 7,7,7,7,7,7,7,7,7,7
     ; db hdeight*screen_w*Bytes_per_pixel+4    dup(7h)
-     dw  100h, 7,102h, 103h , 104h;,  105h,106h,  107h, 108h,109h, 10ah, 10bh, 10ch,10dh,10eh;,10fh;,110h;,111h;,112h;10ch,10dh,10eh,10fh, 8,  8,8,8,8, 110h, 0, 101h,0   ; 7,  7, 7 ,7,7,7,7
+   ;  dw  100h, 7,102h, 103h , 104h;,  105h,106h,  107h, 108h,109h, 10ah, 10bh, 10ch,10dh,10eh;,10fh;,110h;,111h;,112h;10ch,10dh,10eh,10fh, 8,  8,8,8,8, 110h, 0, 101h,0   ; 7,  7, 7 ,7,7,7,7
      ;dw  ,7,7,7,7,7, 7,7,7,7,7,7,7,7,7,7,102h, 103h , 104h,  105h,106h
-      dw  7,102h, 103h , 8,106h,7
-      dw  7,102h, 103h , 8,107h
-      dw   8
-      dw  7,102h, 103h , 8
-      dw  7,102h, 103h , 8
-      dw  7,102h, 103h , 8
-      dw  7,102h, 103h , 8
-      dw  7,102h, 103h , 104h
+    ;  dw  7,102h, 103h , 8,106h,7
+     ; dw  7,102h, 103h , 8,107h
+     ; dw   8
+     ; dw  7,102h, 103h , 8
+     ; dw  7,102h, 103h , 8
+     ; dw  7,102h, 103h , 8
+     ; dw  7,102h, 103h , 8
+     ; dw  7,102h, 103h , 104h
    ;  dw  100h, 7,102h, 103h , 104h,  105h,106h
-     dw  101h                                         ;28+8=36
+     ;dw  101h                                         ;28+8=36
      dw  0ffffh
      db 256 dup(0)
 
+lzw_table:
+        dq      2048 dup(0)
 
+
+lzw_vocab:
+        db      00h,01h,02h,03h,04h,05h,06h,07h,08h,09h,0ah,0bh,0ch,0dh,0eh,0fh
+        db      10h,11h,12h,13h,14h,15h,16h,17h,18h,19h,1ah,1bh,1ch,1dh,1eh,1fh
+        db      20h,21h,22h,23h,24h,25h,26h,27h,28h,29h,2ah,2bh,2ch,2dh,2eh,2fh
+        db      30h,31h,32h,33h,34h,35h,36h,37h,38h,39h,3ah,3bh,3ch,3dh,3eh,3fh
+        db      40h,41h,42h,43h,44h,45h,46h,47h,48h,49h,4ah,4bh,4ch,4dh,4eh,4fh
+        db      50h,51h,52h,53h,54h,55h,56h,57h,58h,59h,5ah,5bh,5ch,5dh,5eh,5fh
+        db      60h,61h,62h,63h,64h,65h,66h,67h,68h,69h,6ah,6bh,6ch,6dh,6eh,6fh
+        db      70h,71h,72h,73h,74h,75h,76h,77h,78h,79h,7ah,7bh,7ch,7dh,7eh,7fh
+        db      80h,81h,82h,83h,84h,85h,86h,87h,88h,89h,8ah,8bh,8ch,8dh,8eh,8fh
+        db      90h,91h,92h,93h,94h,95h,96h,97h,98h,99h,9ah,9bh,9ch,9dh,9eh,9fh
+
+        db      0a0h,0a1h,0a2h,0a3h,0a4h,0a5h,0a6h,0a7h,0a8h,0a9h,0aah,0abh,0ach,0adh,0aeh,0afh
+        db      0b0h,0b1h,0b2h,0b3h,0b4h,0b5h,0b6h,0b7h,0b8h,0b9h,0bah,0bbh,0bch,0bdh,0beh,0bfh
+        db      0c0h,0c1h,0c2h,0c3h,0c4h,0c5h,0c6h,0c7h,0c8h,0c9h,0cah,0cbh,0cch,0cdh,0ceh,0cfh
+        db      0d0h,0d1h,0d2h,0d3h,0d4h,0d5h,0d6h,0d7h,0d8h,0d9h,0dah,0dbh,0dch,0ddh,0deh,0dfh
+        db      0e0h,0e1h,0e2h,0e3h,0e4h,0e5h,0e6h,0e7h,0e8h,0e9h,0eah,0ebh,0ech,0edh,0eeh,0efh
+        db      0f0h,0f1h,0f2h,0f3h,0f4h,0f5h,0f6h,0f7h,0f8h,0f9h,0fah,0fbh,0fch,0fdh,0feh,0ffh
+        dq   8192 dup   (0)
+lzw_code:
+        db      4096 dup(0)
 gif_file_header:
          db      'GIF89a'
 width   dw     screen_w
@@ -103,66 +237,6 @@ copmrbytes         db      7 ;screen_w*hdeight+0 ;0feh ;êîëè÷åñòâî á�
 compressed_picture:
 ;pixels:
         db      256 dup(0)
-      ;    db      7,04h,02h,0eh,04h ;,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-         ; db      0x00, 0x07, 0x08, 0x1C, 0x28, 0x30, 0x20
-       ;     db     0x00, 0x07, 0x08, 0x1c, 0x30, 0x10, 0x0
-          ; 0000 0000 1110 0000 0001 0000 0011 1000 0000 1100 0000 0100
-          ; 000000001 110000000 010000001 110000000 110000000 100
-          ; 000000001 110000000 010000001 110000000 110000001
-          ; 00000000  1110000   00010000  0011 1000 00001100 0000 1000
-          ;  00        07       08        1c      030      10
-          ; 0x08, 0x0B,
-          ; 0x00, 0x35, 0x0C, 0x11, 0x48, 0x24, 0x43,0x41, 0x22, 0x01, 0x01, 0x00, 0x3B,
-          ; 0000 0000  1010 1100  0011 0000  1000 1000  0010 0001  0100 0010  1100 0010  1000 0010  0100 0100 1000 0000 1000 0000
-          ;  000000001 010110000 110000100 010000010 000101000 010110000 101000001 001000100 100000001 0000000
-         ; db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-      ;    db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-      ;    db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-      ;    db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
 
-       ;   db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-       ;   db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-       ;   db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-       ;   db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-       ;   db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-       ;   db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-       ;   db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-       ;   db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-       ;   db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-       ;   db      7,0eh,1Ch;,38h  7,0eh,1Ch,
-        ;  db      255
-        ;  db      7,0eh,1ch, 38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-        ;  db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-        ;  db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-        ;  db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-        ;  db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-
-        ;  db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-        ;  db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-        ;  db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-        ;  db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-        ;  db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-        ;  db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-        ;  db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-        ;  db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-        ;  db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-        ;  db      7,0eh,1Ch;38h ,  7,0eh,1Ch
-        ;  db      255
-        ;   db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-        ;  db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-        ;  db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-        ;  db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-        ;  db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-
-        ;  db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-        ;  db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-        ;  db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-        ;  db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-        ;  db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-        ;  db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-        ;  db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-        ;  db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-        ;  db      7,0eh,1Ch,38h,70h,0E0h,0C0h,81h, 03h, 7,0eh,1Ch,38h,70h,0E0h,0C0h ,81h , 03h
-        ;  db      7,0eh,1Ch;,38h
          db       0,3Bh
 endofgif:
